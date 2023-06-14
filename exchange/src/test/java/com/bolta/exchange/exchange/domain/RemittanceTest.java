69fec4dc4b2179700dd9eb9d7dba836bcb53f5aa
qwerty1434 <qwerty1434@naver.com>
2023-06-14 18:26:57 +0900

    :white_check_mark: Remittance 범위 내 검증 테스트

---
+++ exchange/src/test/java/com/bolta/exchange/exchange/domain/RemittanceTest.java
@@ -0,0 +1,27 @@
+package com.bolta.exchange.exchange.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RemittanceTest {
+
+    @DisplayName("0이상 10_000이하의 값이 주어지면 Remittance 생성 성공")
+    @ParameterizedTest
+    @ValueSource(doubles = {0, 0.1, 134.567, 9_999.999, 10_000.0})
+    void successCreatingRemittanceWhenValidValue(double value){
+        Assertions.assertDoesNotThrow(() -> Remittance.from(value));
+    }
+
+    @DisplayName("0미만, 10_000초과의 값이 주어지면 Remittance 생성 실패")
+    @ParameterizedTest
+    @ValueSource(doubles = {-10,-0.001,10_001,Double.MAX_VALUE})
+    void failCreatingRemittanceWhenInvalidValue(double value){
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> Remittance.from(value));
+    }
+
+}
