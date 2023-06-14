a0022f9f536156e56da747caa67f6cc134f80b10
qwerty1434 <qwerty1434@naver.com>
2023-06-14 18:01:30 +0900

    :sparkles: 송금액 유효성 검사

--- exchange/src/main/java/com/bolta/exchange/exchange/domain/Remittance.java
+++ exchange/src/main/java/com/bolta/exchange/exchange/domain/Remittance.java
@@ -1,14 +1,22 @@
 package com.bolta.exchange.exchange.domain;
 
 public class Remittance {
+    private static final String INVALID_REMITTANCE = "송금액이 올바르지 않습니다.";
     private final double remittance;
 
+
     private Remittance(double remittance) {
+        ValidateThatRemittanceIsWithinRange(remittance);
         this.remittance = remittance;
     }
 
     public static Remittance from(double remittance){
         return new Remittance(remittance);
+    }
+
+    public void ValidateThatRemittanceIsWithinRange(double remittance){
+        if(remittance < 0 || remittance > 10_000) throw
+                new IllegalArgumentException(INVALID_REMITTANCE);
     }
 
     public double getRemittance() {
