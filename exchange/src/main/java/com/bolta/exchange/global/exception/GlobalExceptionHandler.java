7911e23bac42cca56ef37f13c027916827a60ad0
qwerty1434 <qwerty1434@naver.com>
2023-06-19 09:27:04 +0900

    :ambulance: NoSuchElementException에 대한 예외처리 추가

--- exchange/src/main/java/com/bolta/exchange/global/exception/GlobalExceptionHandler.java
+++ exchange/src/main/java/com/bolta/exchange/global/exception/GlobalExceptionHandler.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.reactive.function.client.WebClientRequestException;
 
-import static com.bolta.exchange.global.exception.ErrorMessage.INTERNAL_SERVER_ERROR;
+import java.util.NoSuchElementException;
+
+import static com.bolta.exchange.global.exception.ErrorMessage.*;
 
 @ControllerAdvice
 @Slf4j
@@ -19,6 +21,13 @@ public class GlobalExceptionHandler {
         log.error("api server에서 값을 제대로 받아오지 못했습니다.", e);
         return ResponseEntity.internalServerError()
                 .body(ErrorResponse.of(INTERNAL_SERVER_ERROR));
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    protected ResponseEntity NoSuchElementException(NoSuchElementException e){
+        log.error("api server의 quotes에서 값을 찾을 수 없습니다.", e);
+        return ResponseEntity.internalServerError()
+                .body(ErrorResponse.of(RATE_NOT_FOUND_ERROR));
     }
 
     @ExceptionHandler(WebClientRequestException.class)
@@ -39,7 +48,8 @@ public class GlobalExceptionHandler {
     protected ResponseEntity ConversionFailedException(ConversionFailedException e){
         log.error("사용자가 올바르지 않은 Currency로 요청을 전송했습니다.", e);
         return ResponseEntity.badRequest()
-                .body(ErrorResponse.of(HttpStatus.BAD_REQUEST,e.getMessage()));
+                .body(ErrorResponse.of(INVALID_CURRENCY_VALUE_ERROR));
     }
+
 
 }
