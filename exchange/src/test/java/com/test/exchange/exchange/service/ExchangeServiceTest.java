1366b4e75e284ae2baa644883bf0c5c7e133660c
qwerty1434 <qwerty1434@naver.com>
2023-07-14 20:56:23 +0900

    :white_check_mark: Mokito로 외부API를 모킹한 뒤 Service Layer 테스트

--- exchange/src/test/java/com/test/exchange/exchange/service/ExchangeServiceTest.java
+++ exchange/src/test/java/com/test/exchange/exchange/service/ExchangeServiceTest.java
@@ -1,5 +1,7 @@
 package com.test.exchange.exchange.service;
 
+import com.test.exchange.apilayer.domain.ExchangeRateClient;
+import com.test.exchange.apilayer.dto.ExchangeRateResponse;
 import com.test.exchange.exchange.domain.Currency;
 import com.test.exchange.exchange.domain.Exchange;
 import com.test.exchange.exchange.domain.Remittance;
@@ -7,43 +9,99 @@ import com.test.exchange.exchange.dto.ExchangeMoneyResponse;
 import com.test.exchange.exchange.repository.ExchangeRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Map;
 
 import static com.test.exchange.exchange.domain.Currency.KRW;
 import static com.test.exchange.exchange.domain.Currency.USD;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class ExchangeServiceTest {
     private static final Currency VALID_SOURCE = USD;
     private static final Currency VALID_TARGET = KRW;
-    @Autowired
-    ExchangeService exchangeService;
-    @Autowired
+    @Mock
+    ExchangeRateClient exchangeRateClient;
+    @Mock
     ExchangeRepository exchangeRepository;
+    @InjectMocks
+    ExchangeService exchangeService;
 
-    @DisplayName("Exchange엔티티를 저장합니다.")
+
+    @DisplayName("수취국가와 송금국가를 입력해 원하는 환율을 조회합니다.")
     @Test
-    public void testSaveExchange(){
-        Remittance remittance = Remittance.from(1_000);
-        double exchangeRate = 1.5;
-        Exchange exchange = exchangeService.saveExchange(VALID_SOURCE, VALID_TARGET, remittance, exchangeRate);
+    public void getExchangeRate(){
+        double expectedValue = 1300;
+        Map<String,Double> quotes = Map.of("USDKRW",expectedValue);
+        ExchangeRateResponse response = ExchangeRateResponse.builder()
+                .success(true)
+                .timestamp(0)
+                .source(VALID_SOURCE)
+                .quotes(quotes)
+                .error(null)
+                .build();
 
-        long exchangeId = exchange.getId();
-        Exchange savedExchange = exchangeRepository.findById(exchangeId).orElse(null);
+        BDDMockito.given(exchangeRateClient.getExchangeRate(any(),any(),any(),any(),any(),any()))
+                .willReturn(response);
 
-        assertThat(savedExchange).isNotNull();
+        double exchangeRate = exchangeService.getExchangeRate(VALID_SOURCE, VALID_TARGET);
+
+        assertThat(exchangeRate).isEqualTo(expectedValue);
     }
 
-    @DisplayName("환전을 시도하면 Target Currency로 변환된 돈을 반환합니다.")
+    @DisplayName("환전 시 받게될 금액을 확인할 수 있습니다.")
     @Test
-    public void testExchangeMoneyCurrency(){
-        double givenRemittance = 1_000;
+    public void getCalculatedRemittance(){
+        double exchangeRate = 1300;
+        Map<String,Double> quotes = Map.of("USDKRW",exchangeRate);
+        ExchangeRateResponse response = ExchangeRateResponse.builder()
+                .success(true)
+                .timestamp(0)
+                .source(VALID_SOURCE)
+                .quotes(quotes)
+                .error(null)
+                .build();
+        double givenRemittance = 1000;
+
+        BDDMockito.given(exchangeRateClient.getExchangeRate(any(),any(),any(),any(),any(),any()))
+                .willReturn(response);
+
+        double calculatedRemittance = exchangeService.getCalculatedRemittance(VALID_SOURCE, VALID_TARGET, givenRemittance);
+
+        assertThat(calculatedRemittance).isEqualTo(exchangeRate * givenRemittance);
+    }
+
+
+    @DisplayName("환전을 하면 Target Currency로 변환된 돈을 반환합니다.")
+    @Test
+    public void exchangeMoney(){
+        double exchangeRate = 1300;
+        Map<String,Double> quotes = Map.of("USDKRW",exchangeRate);
+        ExchangeRateResponse response = ExchangeRateResponse.builder()
+                .success(true)
+                .timestamp(0)
+                .source(VALID_SOURCE)
+                .quotes(quotes)
+                .error(null)
+                .build();
+        double givenRemittance = 1000;
+
+        BDDMockito.given(exchangeRateClient.getExchangeRate(any(),any(),any(),any(),any(),any()))
+                .willReturn(response);
+        BDDMockito.given(exchangeRepository.save(any()))
+                .willReturn(Exchange.of(VALID_SOURCE,VALID_TARGET, Remittance.from(givenRemittance),exchangeRate));
+
         ExchangeMoneyResponse exchangeMoneyResponse =
                 exchangeService.exchangeMoney(VALID_SOURCE, VALID_TARGET, givenRemittance);
+
         assertThat(VALID_TARGET).isEqualTo(exchangeMoneyResponse.getCurrency());
-        assertThat(exchangeMoneyResponse.getRemittance()).isNotEqualTo(givenRemittance);
+        assertThat(exchangeMoneyResponse.getRemittance()).isEqualTo(exchangeRate * givenRemittance);
     }
 
 }
